c55498664a02f06fcc7822075e8aba119da56362
Iandomin <92277692+Iandomin@users.noreply.github.com>
2021-11-05 00:39:39 -0600

    Add files via upload

---
+++ uii-act1-main/Articulos.php
@@ -0,0 +1,70 @@
+<?php include('header.php'); ?>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Articulos</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Carnes frias.
+					   Res</p>
+					<br/>
+					<p>Milanesa de Res Pulpa Blanca C/C Rancho Don Francisco Selecta Kg $179.90 </p>
+					<br/>
+					<p>Milanesa de Res Pulpa Bola Rancho Don Francisco Selecta Kg $204.00</p>
+					<br/>
+					<p>Pollo</p>
+					<br/>
+					<p>Pechuga de Pollo sin Piel Kg $119.90</p>
+					<br/>
+					<p>Pierna de Pollo con Muslo Kg $29.90</p>
+					<br/>
+					<p>Cerdo</p>
+					<br/>
+					<p>Chuleta Mixta de Lomo de Cerdo Kg $96.00</p>
+					<br/>
+					<p>Costilla de Cerdo Baby Back Kg $119.00</p>
+					<br/>
+					<p>pescado</p>
+					<br/>
+					<p>Filete de Salmón Descongelado Kg $398.00</p>
+					<br/>
+					<p>Robalito Chucumite Kg $215.00</p>
+					<br/>
+					<p>Frutas y Verduras.
+					   Verduras</p>
+					<br/>
+					<p>Pimiento Mini Campo Vivo Orgánico $76.00</p>
+					<br/>
+					<p>Cebolla Blanca Kg $39.90</p>
+					<br/>
+					<p>Frutas</p>
+					<br/>
+					<p>Manzana Golden Bolsa Kg $49.90</p>
+					<br/>
+					<p>Blueberries Charola con 170 gr $49.90</p>
+					<br/>
+					<p>Bebidas y Jugos</p>
+					<br/>
+					<p>Bebida Arandano Granada Valley Foods 946 ml $21.00</p>
+					<br/>
+					<p>Bebida Boing Mango 354 Ml Bot $10.00</p>
+					<br/>
+					<p>Refresco Peñafiel Mandarina 600 Ml $13.40</p>
+					<br/>
+					<p>Concentrado Natures Factory Té Blanco Arándano 20 Gr $9.40</p>
+
+				</article>
+			</div>
+			<form action="/action_page.php" method="get">
+  <label for="fname">Buscar Articulo:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+    <?php include('sidebar.php'); ?>
+		</div>
+	</section>
+	<?php include('footer.php'); ?>
+</body>
+</html>
---
+++ uii-act1-main/Tiket.php
@@ -0,0 +1,56 @@
+<?php include('header.php'); ?>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Tiket</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Estimado cliente</p>
+					<br/>
+					<p>Se les informa que los tickets de venta, emitidos en las sucursales de la frontera Norte por nuestras cajas registradoras, durante el mes de Enero 2019 
+					   a la fecha, ya pueden ser facturados.</p>
+					<br/>
+					<p>Agradecemos su comprensión y preferencia.</p>
+					<br/>
+					<p>Atentamente
+                       Tiendas Soriana.</p>
+					<br/>
+					<p>Bienvenido al Servicio de Facturación 
+					   Electrónica de Soriana.</p>
+					<br/>
+					<p>Te recordamos que tienes 30 días para 
+					   tramitar tu factura después de realizar tu 
+					   compra.</p>
+					<br/>
+					<p>En el Centro de atención a clientes estamos 
+					   para ayudarte en chat o puedes marcar al 
+					   818329-9252 opción 5 sin costo para ti, 
+					   desde cualquier Ciudad dentro de la 
+					   República Mexicana.</p>
+					<br/>
+					<p>Para visualizar tus facturas, es necesario 
+					   tener instalado Adobe Acrobat Reader 
+					   versión 9.0 o superior.</p>
+					<br/>
+					<p>¿Tienes dudas?
+                       Consulta nuestra sección de preguntas 
+					   frecuentes o envíanos tus preguntas sobre 
+					   facturación al correo: ayuda@soriana.com</p>
+					<br/>
+					<p>Entérate sobre CDFI
+                       Entérate en nuestra sección de Preguntas Frecuentes CFDI</p>
+				</article>
+			</div>
+			<form action="/action_page.php" method="get">
+  <label for="fname">numero del codigo de barras:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+    <?php include('sidebar.php'); ?>
+		</div>
+	</section>
+	<?php include('footer.php'); ?>
+</body>
+</html>
